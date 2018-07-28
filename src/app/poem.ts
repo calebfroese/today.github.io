ba13f5c38794bf769cf0cab3c11261a00d73cbfb
calebfroese <caleb.froese@gmail.com>
2018-07-28 07:48:27 +0000

    Test commit

---
+++ src/app/poem.ts
@@ -0,0 +1,117 @@
+export const poem =
+[ { title: 'At The Cementary', content: 'Rotten stone towers sultrily warmed.
+
+Yellow haze of incense hovers.
+
+Bees hum chaotically swarmed
+
+And the flower trellises shake.
+
+Slowly a breath stirs there
+
+By the sun-still walls,
+
+Dwindles glimmering, like a deceit -
+
+Songs for the dead deeply shiver away.
+
+Long it listens after in the green,
+
+Lets the bushes shine brighter;
+
+Brown swarms of mosquitoes spray
+
+Over old tombstones', url: 'https://www.poemist.com/georg-trakl/at-the-cementary', poet: { name: 'Georg Trakl', url: 'https://www.poemist.com/georg-trakl' } }, { title: 'Te Deum Laudamus', content: 'I will praise God alway for each new year,
+Knowing that it shall be most worthy of
+His kindness and His pity and His love.
+I will wait patient, till, from sphere to sphere,
+Across large times and spaces, ringeth clear
+
+The voice of Him who sitteth high above,
+Saying, “Behold! thou hast had pain enough;
+Come; for thy Love is waiting for thee here!”
+I know that it must happen as God saith.
+I know it well. Yet, also, I know well
+That where birds sing and yellow wild-flowers dwell,
+Or where some strange new sunset lingereth,
+All Earth shall alway of her presence tell
+Who liveth not for me this side of death.', url: 'https://www.poemist.com/francis-joseph-sherman/te-deum-laudamus', poet: { name: 'Francis Joseph Sherman', url: 'https://www.poemist.com/francis-joseph-sherman' } }, { title: 'The Angel\'s Song', content: 'Rolling downward, through the midnight,
+Comes a glorious burst of heav’nly song;
+’Tis a chorus full of sweetness—
+And the singers are an angel throng.
+
+Refrain
+
+“Glory! glory in the highest!
+On the earth goodwill and peace to men!”
+Down the ages send the echo;
+Let the glad earth shout again!
+
+Wond’ring shepherds see the glory,
+Hear the word the shining ones declare;
+At the manger fall in worship,
+While the music fills the quiv’ring air.
+
+Refrain
+
+Christ the Savior, God’s Anointed,
+Comes to earth our fearful debt to pay—
+Man of sorrows, and rejected,
+Lamb of God, that takes our sin away.
+
+Refrain', url: 'https://www.poemist.com/robert-lowry/the-angels-song', poet: { name: ' Robert Lowry', url: 'https://www.poemist.com/robert-lowry' } }, { title: 'Crossroads', content: 'Life is full of crossroads. 
+One around every turn. 
+Sometimes you make the right choice- 
+Sometimes you get burned. Is the path you take for love? 
+Or is it for hate? 
+Careful with the path you pick. 
+For it determines your fate. Some feel the path is chosen for them, 
+And rarely give it a thought. 
+Take a look at your life however- 
+Is it the one you sought? Do not choose lightly. 
+Or walk through life blind. 
+Your life can be an adventure 
+You never know what you will find.', url: 'https://www.poemist.com/antonio-simoneli/crossroads', poet: { name: ' Antonio Simoneli', url: 'https://www.poemist.com/antonio-simoneli' } }, { title: 'A Moral Vindicator', content: 'If Mr. Jones, Lycurgus B.,
+Had one peculiar quality,
+\'Twas his severe advocacy
+Of conjugal fidelity.
+
+His views of heaven were very free;
+His views of life were painfully
+Ridiculous; but fervently
+He dwelt on marriage sanctity.
+
+He frequently went on a spree;
+But in his wildest revelry,
+On this especial subject he
+Betrayed no ambiguity.
+
+And though at times Lycurgus B.
+Did lay his hands not lovingly
+Upon his wife, the sanctity
+Of wedlock was his guaranty.
+
+But Mrs. Jones declined to see
+Affairs in the same light as he,
+And quietly got a decree
+Divorcing her from that L. B.
+
+And what did Jones, Lycurgus B.,
+With his known idiosyncrasy?
+He smiled,--a bitter smile to see,--
+And drew the weapon of Bowie.
+
+He did what Sickles did to Key,--
+What Cole on Hiscock wrought, did he;
+In fact, on persons twenty-three
+He proved the marriage sanctity.
+
+The counselor who took the fee,
+The witnesses and referee,
+The judge who granted the decree,
+Died in that wholesale butchery.
+
+And then when Jones, Lycurgus B.,
+Had wiped the weapon of Bowie,
+Twelve jurymen did instantly
+Acquit and set Lycurgus free.', url: 'https://www.poemist.com/francis-bret-harte/a-moral-vindicator', poet: { name: 'Francis Bret Harte', url: 'https://www.poemist.com/francis-bret-harte' } } ]
