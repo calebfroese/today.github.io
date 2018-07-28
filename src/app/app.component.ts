25833e639957cefe80be52e71b9a2567128a8a82
Caleb Froese <calebfroese@Calebs-iMac.local>
2018-07-28 17:26:24 +0930

    Add poem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   day = 'Sunday';
-  num = 5;
+  poem$: any;
+
+  constructor(public http: Http) {}
+
+  ngOnInit() {
+    this.poem$ = this.http.get('assets/poem.json', {}).pipe(map(data => {
+      const poems = data.json();
+      return poems[0].content;
+    }));
+  }
 }
