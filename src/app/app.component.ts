47f434a9018873562374a4da597ce1809008ec54
calebfroese <caleb.froese@gmail.com>
2018-08-09 14:57:30 +0000

    Update for Thursday

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  day = 'Wednesday';
+  day = 'Thursday';
   poem$: any;
 
   constructor(public http: Http) {}
